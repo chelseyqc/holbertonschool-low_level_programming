17e7826a3e4ed8d01def792f9d750b5d793b9b7a
root <6721@holbertonstudents.com>
2023-06-15 00:26:26 -0700

    fixed task 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,8 +3,10 @@
  * main - Prints with proper grammar, but the outcome
  * is a piece of art,
  * followed by a new line
+ *
+ * Return: 0
  */
-int main(void)
+	int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
