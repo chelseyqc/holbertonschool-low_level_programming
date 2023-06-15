524418594317fb97b75bf816a8a13421fabc4832
root <6721@holbertonstudents.com>
2023-06-14 23:38:31 -0700

    fixed task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,10 +3,10 @@
  * main - Prints "Programming is like building a
  * multlingual puzzle, followed by a new line.
  *
- * Return: Always 0.
+ * Return: 0.
  */
 	int main(void)
 {
-	puts("\Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
