963a2f3835b3e0d772c362ceea0d5727107a5a4c
root <6721@holbertonstudents.com>
2023-06-18 20:58:16 -0700

    added answer to task 1

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - assigns a random number to variable n
+ * to print if the last digit is greater than 5,
+ * is 0, or is less than 6 and not 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	int a;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	a = n % 10;
+
+	if (a > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, a);
+	}
+	else if (a == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, a);
+	}
+	else if (a < 6 && a != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
+	}
+
+	return (0);
+}
--- variables_if_else_while/README.md
+++ variables_if_else_while/README.md
@@ -1,2 +1,5 @@
 # Script descriptions
 [0-positive_or_negative.c](https://github.com/chelseyqc/holbertonschool-low_level_programming/blob/master/variables_if_else_while/0-positive_or_negative.c) - A C program that assigns a random number to the variable `n` and prints whether the number stored is positive or negative
+
+
+[1-last_digit.c](https://github.com/chelseyqc/holbertonschool-low_level_programming/tree/master/variables_if_else_while/1-last_digit.c) - A C program that assigns a random number to the variable `n` and prints whether the last digit of the number stored is greater than 5, is 0 or is less than 6 and not 0
