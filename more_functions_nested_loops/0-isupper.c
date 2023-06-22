874ab8a0830964c5a0f747a2f033c7c16a21ed2a
root <6721@holbertonstudents.com>
2023-06-22 00:51:30 -0700

    fixed task 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* _isupper -  checks for uppercase character
+* _isupper - checks for uppercase character
 * @c: input character
 *
 * Return: 1 if true or 0 if false
 */
-int _isupper(int c);
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'z')
 	{
