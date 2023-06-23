cc3be333b982f1b75de96da5c5b5fec8efbe0add
root <6721@holbertonstudents.com>
2023-06-22 18:25:55 -0700

    added answer to task 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* main - prints numbers from 1 to 100, multiples of 3 prints 'Fizz'
+* and multiples of 5 prints 'Buzz'
+*
+* Return: 0
+*/
+int main(void)
+{
+	int number;
+
+	number = 1;
+	while (number <= 100)
+	{
+		if (number % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (number % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%i ", number);
+		}
+		number = number + 1;
+	}
+	printf("\n");
+	return (0);
+}
