1b3b302646320d1ac647c3e800313f60453cece6
root <6721@holbertonstudents.com>
2023-06-28 19:11:36 -0700

    added answer to task 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strchr - locates a character in a string
+* @s: the string
+* @c: the character in the string
+*
+* Return: a pointer to the first occurance of c or NULL if not found
+*/
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	index = 0;
+	while (s[index] != '\0')
+	{
+		if (s[index] == c)
+		{
+			return (&(s[index]));
+		}
+		index = index + 1;
+	}
+	return ('\0');
+}
