763be3ab2917213352c44afa36f272468b1abc74
root <6721@holbertonstudents.com>
2023-06-29 00:04:24 -0700

    added answer to task 6

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_chessboard - prints the chessboard
+* @a: 
+*
+* Return: void
+*/
+void print_chessboard(char (*a)[8])
+{
+	int index_a;
+	int index_b;
+
+	index_a = 0;
+	while (index_a < 8)
+	{
+		while (index_b < 8)
+		{
+			_putchar(a[index_a][index_b]);
+			index_b = index_b + 1;
+		}
+		_putchar('\n');
+		index_a = index_a + 1;
+		index_b = 0;
+	}
+}
