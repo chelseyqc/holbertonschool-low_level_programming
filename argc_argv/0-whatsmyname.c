254a869666f2821690c9d974388e54985a53852f
root <6721@holbertonstudents.com>
2023-07-03 23:02:04 -0700

    added answer to task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - program prints its name, followed by a new line
+* @argc: count of arguments
+* @argc: array of pointers to strings
+*
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
