c2e0966f4dd218563f10584aa207d1f10eb6958e
root <6721@holbertonstudents.com>
2023-07-17 00:55:58 -0700

    added README, header and task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+* print_name - call a function to print a name
+* @name: name of the person
+* @f: the pointer to call the function
+*
+* Return: void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+	{
+		f(name);
+	}
+}
