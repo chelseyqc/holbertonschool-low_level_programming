ba7a1748b3eca3afb83fa272b35b66915aaa6e6f
root <6721@holbertonstudents.com>
2023-07-16 17:33:17 -0700

    added README and dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+* struct dog - description of the dog
+* @name: name of the dog
+* @age: ages of the dog
+* @owner: owner of the dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
