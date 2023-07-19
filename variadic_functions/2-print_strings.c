a5ee003824998f30f19d2cbb34cff4ecd594bfe2
root <6721@holbertonstudents.com>
2023-07-18 20:33:50 -0700

    added else statement

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -24,7 +24,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("(nil)");
 		}
-		printf("%s", strings);
+		else
+		{
+			printf("%s", strings);
+		}
 		if (i < (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
