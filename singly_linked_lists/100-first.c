76908a2dbdc2edd8ed7ce12297b8704afa0adc6e
root <6721@holbertonstudents.com>
2023-07-30 23:54:06 -0700

    feat: added print_first

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* print_first - a function that prints before the main is executed
+*
+* Return: void
+*/
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
