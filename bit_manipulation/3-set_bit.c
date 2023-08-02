ae85accdb0c38514e1852ccfb57d7709b2bfaa51
root <6721@holbertonstudents.com>
2023-08-02 02:09:14 -0700

    feat: added set_bit

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* set_bit
+* @n: the input number
+* @index: the given index
+*
+* Return: 1 if worked, else -1
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i;
+
+	if (index > (sizeof(*n) * 8)) /* checks if index is > bit total num */
+	{
+		return (-1); /* did not work */
+	}
+	i = 1; /* initialise to 1 */
+	i = i << index; /* shifts value of i by index */
+	*n = *n | i; /* set the bit at i to 1 */
+	return (1); /* it worked */
+}
