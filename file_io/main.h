8f90bc951fb29239fa8732bea9e15b7262c4aaee
root <6721@holbertonstudents.com>
2023-08-06 19:10:19 -0700

    doc: updated header

--- file_io/main.h
+++ file_io/main.h
@@ -1,5 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include <stddef.h>
+#include <sys/types.h>
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
