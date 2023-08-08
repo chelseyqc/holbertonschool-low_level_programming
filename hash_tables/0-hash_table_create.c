242c4c5bbe4898f3f0651710cec7d4961595eeca
root <6721@holbertonstudents.com>
2023-08-07 17:03:32 -0700

    feat: added hash_table_create

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* hash_table_create - creates a hash table
+* @size: the size of the array
+*
+* Return: a pointer to the newly created hash table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int index;
+	hash_table_t *table;
+
+	index = 0;
+	if (size = 0) /* size = 0 means hash table is invalid */
+	{
+		return (NULL);
+	}
+	table = malloc(sizeof(*table)); /* alloc mem for hash table */
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+	table->array = malloc(sizeof(*table)); /* alloc mem for array */
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+	/* alloc mem for array of pointers that will hold hash table entries */
+	table->array = malloc(sizeof(*(table->array)) * size);
+	if (table->array == NULL)
+	{
+		free(table); /* free the alloc mem */
+		return (NULL);
+	}
+	while (index < size)
+	{
+		table->array[index] = NULL;
+		index = index + 1;
+	}
+	table->size = size; /* store size of hash table */
+	return (table);
+}
